d1783dbf0b4c68f1dbca6b5495092fefa025133f
aaron <pleaseshutupaaron@gmail.com>
2018-01-07 21:04:52 -0500

    trying to get action php working8

--- send_form_email.php
+++ send_form_email.php
@@ -1,7 +1,44 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "https://www.w3.org/TR/html4/strict.dtd">
+<!-- saved from url=(0138)https://mail.google.com/mail/u/1/?ui=2&ik=b7e89d77da&jsver=veKV0pjIDTc.en.&view=pt&msg=160d3637feb65871&search=inbox&siml=160d3637feb65871 -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><style type="text/css" nonce="">
+body,td,div,p,a,input {font-family: arial, sans-serif;}
+</style><meta http-equiv="X-UA-Compatible" content="IE=edge"><title>Gmail - Project Banana</title><style type="text/css" nonce="">
+html {background-color: #3eb7d2} body, td {font-size:13px} a:link, a:active {color:#1155CC; text-decoration:none} a:hover {text-decoration:underline; cursor: pointer} a:visited{color:##6611CC} img{border:0px} pre { white-space: pre; white-space: -moz-pre-wrap; white-space: -o-pre-wrap; white-space: pre-wrap; word-wrap: break-word; max-width: 800px; overflow: auto;} .logo { left: -7px; position: relative; }
+</style></head><body>
+<table align="center" border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" id="m_-9222482336791725274bodyTable" style="border-collapse:collapse;height:100%;margin:0;padding:0;width:100%;background-color:#3eb7d2">
+<tbody><tr>
+<td align="center" valign="top" id="m_-9222482336791725274bodyCell" style="height:100%;margin:0;padding:10px;width:100%;border-top:0">
+<table border="0" cellpadding="0" cellspacing="0" width="100%" class="m_-9222482336791725274templateContainer" style="border-collapse:collapse;border:0;max-width:600px!important">
+<tbody><tr>
+<td valign="top" id="m_-9222482336791725274templatePreheader" style="background:#transparent none no-repeat center/cover;background-color:#transparent;background-image:none;background-repeat:no-repeat;background-position:center;background-size:cover;border-top:0;border-bottom:0;padding-top:9px;padding-bottom:9px"><table border="0" cellpadding="0" cellspacing="0" width="100%" class="m_-9222482336791725274mcnImageBlock" style="min-width:100%;border-collapse:collapse">
+<tbody class="m_-9222482336791725274mcnImageBlockOuter">
+<tr>
+<td valign="top" style="padding:0px" class="m_-9222482336791725274mcnImageBlockInner">
+<table align="left" width="100%" border="0" cellpadding="0" cellspacing="0" class="m_-9222482336791725274mcnImageContentContainer" style="min-width:100%;border-collapse:collapse">
+<tbody><tr>
+<td class="m_-9222482336791725274mcnImageContent" valign="top" style="padding-right:0px;padding-left:0px;padding-top:0;padding-bottom:0;text-align:center">
+<img align="center" alt="" src="./Gmail - Project Banana_files/qLpwyeysp0oQPaW9vjKhx_ntodZpydtEKnb311UEFqKYUIBnhgIdoojuFPtvcM2oo1zETR0kfPGctnHg-jrcNDkkVe6xszYT1lSMUldTfI0WeFwvGF9kIaEGomFT9dlme4puDNzA6PtFRO6WBSe7RDpjGZ9qOZcTY_x6wh4=s0-d-e1-ft" width="238" style="max-width:476px;padding-bottom:0;display:inline!important;vertical-align:bottom;border:0;height:auto;outline:none;text-decoration:none" class="m_-9222482336791725274mcnImage">
+</td>
+</tr>
+</tbody></table>
+</td>
+</tr>
+</tbody>
+</table></td>
+</tr>
+<tr>
+<td valign="top" id="m_-9222482336791725274templateHeader" style="background:#ffffff none no-repeat center/cover;background-color:#ffffff;background-image:none;background-repeat:no-repeat;background-position:center;background-size:cover;border-top:0;border-bottom:0;padding-top:9px;padding-bottom:0"><table border="0" cellpadding="0" cellspacing="0" width="100%" class="m_-9222482336791725274mcnTextBlock" style="min-width:100%;border-collapse:collapse">
+<tbody class="m_-9222482336791725274mcnTextBlockOuter">
+<tr>
+<td valign="top" class="m_-9222482336791725274mcnTextBlockInner" style="padding-top:9px">
+<table align="left" border="0" cellpadding="0" cellspacing="0" style="max-width:100%;min-width:100%;border-collapse:collapse" width="100%" class="m_-9222482336791725274mcnTextContentContainer">
+<tbody><tr>
+<td valign="top" class="m_-9222482336791725274mcnTextContent" style="padding-top:0;padding-right:18px;padding-bottom:9px;padding-left:18px;word-break:break-word;color:#202020;font-family:Helvetica;font-size:16px;line-height:150%;text-align:left">
+<h1 style="text-align:center;display:block;margin:0;padding:0;color:#202020;font-family:Helvetica;font-size:26px;font-style:normal;font-weight:bold;line-height:125%;letter-spacing:normal">Hello again. Glad you could make it.</h1>
 <?php
 
 	$name = $_POST['name'];
-	$title = $_POST['name'];
+	$title = $_POST['title'];
 	$email_to = $_POST['email'];
 
     echo $name0;
@@ -9,3 +46,109 @@
     echo $title;
 
 ?>
+&nbsp;&nbsp;
+<p style="margin:10px 0;padding:0;color:#202020;font-family:Helvetica;font-size:16px;line-height:150%;text-align:left">&nbsp; &nbsp;Wow,&nbsp;I can't believe that NAME, TITLE at MailChimp, is viewing my project! What an honor!<br>
+<br>
+&nbsp; &nbsp;While this isn't exactly the next Facebook, I learned PHP and executed deploying this small project just in one weekend! I am a quick learner, and if given the opportunity, I would love to show you how I can become a productive and vital member of your team.<br>
+<br>
+&nbsp; &nbsp;Please feel free to give me a call at <a href="tel:(706)%20352-1039" value="+17063521039" target="_blank">706-352-1039</a> or email me at <a href="mailto:The.Aaron.Lee.Brooks@gmail.com" target="_blank">The.Aaron.Lee.Brooks@gmail.com</a><wbr>. My portfolio is at AaronLeeBrooks.com, and my LinkedIn and Github are both just AaronLeeBrooks. Thank you again for taking the time to check out this project and my page, and I look forward to hearing from you!</p>
+</td>
+</tr>
+</tbody></table>
+</td>
+</tr>
+</tbody>
+</table></td>
+</tr>
+<td align="center" valign="top" id="m_-5097230901504109588templateFooter" style="background:#333333 none no-repeat center/cover;background-color:#333333;background-image:none;background-repeat:no-repeat;background-position:center;background-size:cover;border-top:0;border-bottom:0;padding-top:45px;padding-bottom:63px">
+<table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" class="m_-5097230901504109588templateContainer" style="border-collapse:collapse;max-width:600px!important">
+<tbody><tr>
+<td valign="top" class="m_-5097230901504109588footerContainer" style="background:transparent none no-repeat center/cover;background-color:transparent;background-image:none;background-repeat:no-repeat;background-position:center;background-size:cover;border-top:0;border-bottom:0;padding-top:0;padding-bottom:0"><table border="0" cellpadding="0" cellspacing="0" width="100%" class="m_-5097230901504109588mcnFollowBlock" style="min-width:100%;border-collapse:collapse">
+<tbody class="m_-5097230901504109588mcnFollowBlockOuter">
+<tr>
+<table border="0" cellpadding="0" cellspacing="0" width="100%" class="m_-5097230901504109588mcnFollowContentContainer" style="min-width:100%;border-collapse:collapse">
+<tbody><tr>
+<td align="center" style="padding-left:9px;padding-right:9px">
+<table border="0" cellpadding="0" cellspacing="0" width="100%" style="min-width:100%;border-collapse:collapse" class="m_-5097230901504109588mcnFollowContent">
+<tbody><tr>
+<td align="center" valign="top" style="padding-top:9px;padding-right:9px;padding-left:9px">
+<table align="center" border="0" cellpadding="0" cellspacing="0" style="border-collapse:collapse">
+<tbody><tr>
+<td align="center" valign="top">
+<table align="left" border="0" cellpadding="0" cellspacing="0" style="display:inline;border-collapse:collapse">
+<tbody><tr>
+<td valign="top" style="padding-right:10px;padding-bottom:9px" class="m_-5097230901504109588mcnFollowContentItemContainer">
+<table border="0" cellpadding="0" cellspacing="0" width="100%" class="m_-5097230901504109588mcnFollowContentItem" style="border-collapse:collapse">
+<tbody><tr>
+<td align="left" valign="middle" style="padding-top:5px;padding-right:10px;padding-bottom:5px;padding-left:9px">
+<table align="left" border="0" cellpadding="0" cellspacing="0" width="" style="border-collapse:collapse">
+<tbody><tr>
+</td>
+</tr>
+</tbody></table>
+</td>
+</tr>
+</tbody></table>
+</td>
+</tr>
+</tbody></table>
+</td>
+</tr>
+</tbody></table>
+</td>
+</tr>
+</tbody></table>
+</td>
+</tr>
+</tbody></table>
+</td>
+</tr>
+</tbody>
+</table><table border="0" cellpadding="0" cellspacing="0" width="100%" class="m_-5097230901504109588mcnDividerBlock" style="min-width:100%;border-collapse:collapse;table-layout:fixed!important">
+<tbody class="m_-5097230901504109588mcnDividerBlockOuter">
+<tr>
+<td class="m_-5097230901504109588mcnDividerBlockInner" style="min-width:100%;padding:18px">
+<table class="m_-5097230901504109588mcnDividerContent" border="0" cellpadding="0" cellspacing="0" width="100%" style="min-width:100%;border-top:2px solid #505050;border-collapse:collapse">
+<tbody><tr>
+<td>
+</td>
+</tr>
+</tbody></table>
+</td>
+</tr>
+</tbody>
+</table><table border="0" cellpadding="0" cellspacing="0" width="100%" class="m_-5097230901504109588mcnTextBlock" style="min-width:100%;border-collapse:collapse">
+<tbody class="m_-5097230901504109588mcnTextBlockOuter">
+<tr>
+<td valign="top" class="m_-5097230901504109588mcnTextBlockInner" style="padding-top:9px">
+<table align="left" border="0" cellpadding="0" cellspacing="0" style="max-width:100%;min-width:100%;border-collapse:collapse" width="100%" class="m_-5097230901504109588mcnTextContentContainer">
+<tbody><tr>
+<td valign="top" class="m_-5097230901504109588mcnTextContent" style="padding-top:0;padding-right:18px;padding-bottom:9px;padding-left:18px;word-break:break-word;color:#ffffff;font-family:Helvetica;font-size:12px;line-height:150%;text-align:center">
+<em>Copyright © 2018 Aaron Brooks, Some rights reserved.</em>
+<br>
+You are receiving this email because I think you guys are great and I would love the opportunity to show you what I'm worth.
+<br>
+<br>
+<strong>My mailing address is:</strong>
+<br>
+<div class="m_-5097230901504109588vcard"><span class="m_-5097230901504109588org m_-5097230901504109588fn">Aaron Brooks</span><div class="m_-5097230901504109588adr"><div class="m_-5097230901504109588street-address"><a style="color: white;" href="https://maps.google.com/?q=390+N+Chase+St.Athens,+Ga+30606&amp;entry=gmail&amp;source=g">390 N Chase St.</a></div><span class="m_-5097230901504109588locality"><a style="color: white;" href="https://maps.google.com/?q=390+N+Chase+St.Athens,+Ga+30606&amp;entry=gmail&amp;source=g">Athens</a></span><a style="color: white;" href="https://maps.google.com/?q=390+N+Chase+St.Athens,+Ga+30606&amp;entry=gmail&amp;source=g">, </a><span class="m_-5097230901504109588region"><a style="color: white;" href="https://maps.google.com/?q=390+N+Chase+St.Athens,+Ga+30606&amp;entry=gmail&amp;source=g">Ga</a></span> <span class="m_-5097230901504109588postal-code"><a style="color: white;" href="https://maps.google.com/?q=390+N+Chase+St.Athens,+Ga+30606&amp;entry=gmail&amp;source=g">30606</a></span></div><br></div>
+Want to contact me?<br>
+You can <a href="http://aaronleebrooks.com" style="color:#ffffff;font-weight:normal;text-decoration:underline" target="_blank">check out my portfolio</a> or <a href="./Gmail - Project Banana_files/Aaron Brooks - HD Dev Resume.pdf" style="color:#ffffff;font-weight:normal;text-decoration:underline" target="_blank">gander at my resume</a>.
+<br>
+<br>
+<a href="http://www.mailchimp.com/monkey-rewards/?utm_source=freemium_newsletter&amp;utm_medium=email&amp;utm_campaign=monkey_rewards&amp;aid=df5e4d40570bbdcc40d6bb22d&amp;afl=1" target="_blank" data-saferedirecturl="https://www.google.com/url?hl=en&amp;q=http://www.mailchimp.com/monkey-rewards/?utm_source%3Dfreemium_newsletter%26utm_medium%3Demail%26utm_campaign%3Dmonkey_rewards%26aid%3Ddf5e4d40570bbdcc40d6bb22d%26afl%3D1&amp;source=gmail&amp;ust=1515387875808000&amp;usg=AFQjCNEXTrbSVoqWfOaSJj5srzYrW9IUvQ"><img src="./Gmail - Project Banana_files/EWG2_aArcaOvssP3SFslYqzhOMUDfVY2IAhF8d8mCeeYx7KamZhmNANawB-2bB7YU189SBT2-ICi565CiWZ0NhEH8cRyTB1qfYAiE04jiVKKfBe6hR-L-cFhYc4=s0-d-e1-ft" border="0" alt="Email Marketing Powered by MailChimp" title="MailChimp Email Marketing" width="139" height="54"></a>
+</td>
+</tr>
+</tbody></table>
+</td>
+</tr>
+</tbody>
+</table></td>
+</tr>
+</tbody></table>
+</td>
+</tr>
+</tbody></table>
+</center>
+<img src="./Gmail - Project Banana_files/7j8GOFylz9mzzbkcfRvIjUvLZlAdavKGnSI8MN3ffWYJ6mSvzzMX9yK2XzdiCVkJPRT97v7WYigaug9RoVLkwfQ1fexFdoA6xErxHp4FdHwvU_pyPLuMqPYCo8Zxz3XAkkvtLRDFZnROQ7PJt33mIaAoFUzC1t2A-mHppk3LTffiqc2JIX5i=s0-d-e1-ft" height="1" width="1"></div>
+</font></div></td></tr></tbody></table></td></tr></tbody></table></div></div></body></html>
