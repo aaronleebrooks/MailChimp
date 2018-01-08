0f4896605747ff34e7625715276f69e29127dd07
aaron <pleaseshutupaaron@gmail.com>
2018-01-07 20:45:56 -0500

    trying to get action php working8

--- send_form_email.php
+++ send_form_email.php
@@ -1,11 +1,5 @@
 <?php
-
-	$name = $_POST['name'];
-	$title = $_POST['name'];
-	$email_to = $_POST['email'];
-
-    echo $name;
-    echo $email_to;
-    echo $title;
-}
+    echo '$name';
+    echo '$email_to';
+    echo '$title';
 ?>
